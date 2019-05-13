73cd4f39bbb6405335184d61577dcfb51cd05a15
SamRaf <samrafrafi93@gmail.com>
2019-05-13 15:44:03 +0200

    mon commit

--- test.js
+++ test.js
@@ -1,10 +1,33 @@
-function plusUn(arr) {
-    for(var i = 0; i < arr.length; i++) {
-        for(var j = 0; j < arr[i].length; j++) {
-            arr[i][j] += 1;
-        }
-    }
+// function randomize(arr) {
+//     let temp = 0;
+//     let random = 0;
+    
+
+//     for (var i = 0; i < arr.length; i++) {
+//         random = Math.floor(Math.random() * arr.length);
+//         temp = arr[i];
+//         arr[i] = arr[random];
+//         arr[random] = temp;
+//     } 
+//     return arr;
+// }
+
+
+// console.log(randomize([1,2,3,4,5]));
+
+
+
+
+function randomize(arr) {
+
+    for (var i = 0; i < arr.length; i++) {
+        const random = Math.floor(Math.random() * arr.length);
+        [arr[i],arr[random]] = [arr[random],arr[i]];
+    } 
     return arr;
 }
 
-console.log(plusUn([[1,2],[3,4],[5,6]]));
+
+console.log(randomize([1,2,3,4,5]));
+
+
